27a35f437659934f03cc8a03b97c97586f27fbd8
SherryYuan6 <qyuan7@pratt.edu>
2026-02-25 15:23:24 -0500

    Started script, adjusted camera filters

--- CaveGame/Assets/Scenes/Scripts/NightVisionCam.cs
+++ CaveGame/Assets/Scenes/Scripts/NightVisionCam.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class NightVisionCam : MonoBehaviour
 {
+    public int photosTaken = 0;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,26 @@ public class NightVisionCam : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        TakePhoto();
+    }
+
+    public void TakePhoto()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+
+            //raycast from the camera's position forward
+            //scan for tag of painting
+            //if painting, increase counter of photos taken of that painting
+            //UI update to show how many photos have been taken of each painting
+            // RaycastHit hit;
+
+        }
+
+    }
+
+    public void UpdatePhotoUI()
+    {
+
     }
 }
