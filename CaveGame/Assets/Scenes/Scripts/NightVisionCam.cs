f406dd75e8e7f6e8b2f5e457890a4c8eb1441a28
Emsofy <emmasofgri@gmail.com>
2026-02-28 18:18:57 -0500

    Fixed Raycast and added UI counter

--- CaveGame/Assets/Scenes/Scripts/NightVisionCam.cs
+++ CaveGame/Assets/Scenes/Scripts/NightVisionCam.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
 using System.Collections;
+using TMPro;
 
 public class NightVisionCam : MonoBehaviour
 {
     public int photosTaken = 0; //Note to future self, make sure a play can't just take 5 photos of the same cave painting
-    public float rayDistance = 5f;
+    public float rayDistance = 20f;
     public string cavePaintingTag = "Cave Painting";
+    public string capturedTag = "Untagged";
     public float duration = 0.5f;
+    public TextMeshProUGUI photoCounter;
     public Light lightFlash;
     public AudioSource flashAudio;
+    public Camera scannerCam;
+    public LayerMask paintingLayer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -25,20 +30,24 @@ public class NightVisionCam : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.F))
         {
+            //Debug.DrawLine();
             Debug.Log("Pressed camera button");
             //raycast from the camera's position forward
-            Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+            Ray ray = scannerCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
             RaycastHit hit;
             flashAudio.Play();
             StartCoroutine(Flash());
-            if (Physics.Raycast(ray, out hit, rayDistance))
+            if (Physics.Raycast(ray, out hit, rayDistance, paintingLayer))
             {
+                Debug.DrawRay(ray.origin, ray.direction * rayDistance, Color.red, 2f);
                 //scan for tag of painting
                 if (hit.collider.CompareTag(cavePaintingTag))
                 {
                    Debug.Log("Took photo of: " + hit.collider.name); 
                     photosTaken++;
                     UpdatePhotoUI();
+                    hit.collider.gameObject.tag = capturedTag;
+                    Debug.Log("Changed tag to: " + hit.collider.gameObject.tag);
                 
                 }
 
@@ -55,7 +64,15 @@ public class NightVisionCam : MonoBehaviour
         }
 
     }
-    
+    void OnDrawGizmosSelected()
+    {
+        // Set the color of the gizmo to red
+        Gizmos.color = Color.yellow;
+
+        // Draw a ray starting from the object's position and going forward
+        // transform.forward provides the direction vector relative to the object's rotation
+        Gizmos.DrawRay(scannerCam.transform.position, scannerCam.transform.forward * rayDistance);
+    }
     IEnumerator Flash()
     {
         lightFlash.intensity = 2f;
@@ -64,6 +81,6 @@ public class NightVisionCam : MonoBehaviour
     }
     public void UpdatePhotoUI()
     {
-
+        photoCounter.text = ": " + photosTaken.ToString() + "/5";
     }
 }
